a3b63cf00c514ab229956962cf66070705cf64d1
maart <maart@DESKTOP-R5T2CB9>
2020-08-05 22:08:54 +0100

    Added Observation and Interaction in JSON Parser

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class Main {
@@ -20,6 +21,8 @@ public class Main {
     Charcter[] charcters;
     Charcter currentCharcter;
 
+    Entity[] entities;
+    Item[] items;
     Scene[] scenes;
 
     public Charcter getCurrentCharcter() {
@@ -31,16 +34,30 @@ public class Main {
 
         JSONArray jCharacters = (JSONArray) jo.get("Characters");
         charcters = new Charcter[jCharacters.size()];
-
         for (int i = 0; i < jCharacters.size(); i++){
             JSONObject jCharacter = (JSONObject) jCharacters.get(i);
             Charcter charcter = new Charcter((String) jCharacter.get("Name"));
             charcters[i] = charcter;
         }
 
+        JSONArray jEntities = (JSONArray) jo.get("Entities");
+        entities = new Entity[jEntities.size()];
+        for (int i = 0; i < jEntities.size(); i++){
+            JSONObject jEntity = (JSONObject) jEntities.get(i);
+            Entity entity = new Entity((String) jEntity.get("Name"), loadObservationTexts(jEntity));
+            entities[i] = entity;
+        }
+
+        JSONArray jItems = (JSONArray) jo.get("Entities");
+        items = new Item[jItems.size()];
+        for (int i = 0; i < jItems.size(); i++){
+            JSONObject jItem = (JSONObject) jItems.get(i);
+            Item item = new Item((String) jItem.get("Name"), loadObservationTexts(jItem));
+            items[i] = item;
+        }
+
         JSONArray jScenes = (JSONArray) jo.get("Scenes");
         scenes = new Scene[jScenes.size()];
-
         for (int i = 0; i < jScenes.size(); i++){
 
             JSONObject jScene = (JSONObject) jScenes.get(i);
@@ -52,46 +69,127 @@ public class Main {
 
                 JSONObject jPosition = (JSONObject) jPositions.get(j);
 
-                JSONArray jEntities = (JSONArray) jPosition.get("Enities");
-                Entity[] entities = new Entity[jEntities.size()];
+                JSONArray jEntitiesNames = (JSONArray) jPosition.get("Enities");
+                Entity[] posEntities = new Entity[jEntities.size()];
 
-                for (int k = 0; k < jEntities.size(); k++){
-                    JSONObject jEntity = (JSONObject) jEntities.get(k);
-
-                    Entity entity = new Entity((String) jEntity.get("Name"), loadTextOutputs(jEntity));
-                    entities[k] = entity;
+                for (int k = 0; k < jEntitiesNames.size(); k++){
+                    for (Entity entity :entities) {
+                        if(entity.name.equals((String) jEntitiesNames.get(k))){
+                            posEntities[k] = entity;
+                            break;
+                        }
+                    }
                 }
 
-                Position position = new Position((String) jPosition.get("Name"), entities, loadTextOutputs(jPosition));
+                Position position = new Position((String) jPosition.get("Name"), posEntities, loadObservationTexts(jPosition));
                 positions[j] = position;
             }
 
             Scene scene = new Scene((String) jScene.get("Name"), positions);
             scenes[i] = scene;
         }
+
+        for (Charcter character : charcters) {
+            if(character.name.equals((String) jo.get("StartCharacter"))){
+                currentCharcter = character;
+            }
+        }
     }
 
-    private TextOutput[] loadTextOutputs(JSONObject jObject){
+    private ObservationText[] loadObservationTexts(JSONObject jObject){
 
-        JSONArray jTextouts = (JSONArray) jObject.get("TextOut");
-        TextOutput[] textOutputs = new TextOutput[jTextouts.size()];
+        JSONArray jObservationTexts = (JSONArray) jObject.get("ObservationTexts");
+        ObservationText[] observationTexts = new ObservationText[jObservationTexts.size()];
 
-        for (int t = 0; t < jTextouts.size(); t++){
-            JSONObject jTextout = (JSONObject) jTextouts.get(t);
+        for (int t = 0; t < jObservationTexts.size(); t++){
+            JSONObject jObservationText = (JSONObject) jObservationTexts.get(t);
 
-            JSONArray jConditions = (JSONArray) jTextout.get("Conditions");
-            TextCondition[] textConditions = new TextCondition[jConditions.size()];
+            JSONArray jConditions = (JSONArray) jObservationText.get("Conditions");
+            ObservationCondition[] observationConditions = new ObservationCondition[jConditions.size()];
 
             for (int c = 0; c < jConditions.size(); c++) {
                 JSONObject jContition = (JSONObject) jConditions.get(c);
-                TextCondition textCondition = new TextCondition((String) jContition.get("Name"));
-                textConditions[c] = textCondition;
+
+                ObservationCondition observationCondition = null;
+
+                String name = (String) jContition.get("Name");
+                switch (name){
+                    case"Base":
+                        observationCondition = new ObservationCondition();
+                        break;
+                    case"HasItem":
+                        for (Item item : items) {
+                            if(item.name.equals((String) jContition.get("ItemName"))){
+                                observationCondition = new HasItemCondition(item);
+                                break;
+                            }
+                        }
+                        break;
+                }
+
+                observationConditions[c] = observationCondition;
             }
 
-            TextOutput textOutput = new TextOutput((String) jTextout.get("Text"), textConditions);
-            textOutputs[t] = textOutput;
+            ObservationText observationText = new ObservationText((String) jObservationText.get("Text"), observationConditions);
+            observationTexts[t] = observationText;
         }
-        return textOutputs;
+        return observationTexts;
+    }
+
+    private void loadInteractions(JSONObject jObject){
+        JSONArray jInteractions = (JSONArray) jObject.get("Interactions");
+
+        for (int t = 0; t < jInteractions.size(); t++){
+            JSONObject jInteraction = (JSONObject) jInteractions.get(t);
+
+            JSONArray jActions = (JSONArray) jInteraction.get("Actions");
+            Action[] actions = new Action[jActions.size()];
+
+            for (int c = 0; c < jActions.size(); c++) {
+                String actionString = (String) jActions.get(c);
+                String[] parts = actionString.split(":");
+
+                Item item = null;
+                for (Item testItem: items) {
+                    if(testItem.name == parts[1]){
+                        item = testItem;
+                    }
+                }
+
+                Action action = null;
+
+                switch (parts[0]){
+                    case"ADDI":
+                        action = new AddItemAction(item);
+                        break;
+                    case"RMI":
+                        action = new RemoveItemAction(item);
+                        break;
+                }
+
+                actions[c] = action;
+            }
+
+            Interaction interaction = new Interaction((String) jInteraction.get("Text"), actions);
+
+            JSONArray jObjects = (JSONArray) jInteraction.get("Objects");
+
+            for (int i = 0; i < 2; i++){
+                String name = (String) jObjects.get(i);
+
+                for (Item item : items){
+                    if(item.name.equals(name)){
+                        item.interactions.add(interaction);
+                    }
+                }
+
+                for (Entity entity : entities){
+                    if(entity.name.equals(name)){
+                        entity.interactions.add(interaction);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -118,62 +216,109 @@ class Scene{
 class Position{
     String name;
     Entity[] entities;
-    TextOutput[] textOutputs;
+    ObservationText[] observationTexts;
 
-    public Position(String name, Entity[] entities, TextOutput[] textOutputs) {
+    public Position(String name, Entity[] entities, ObservationText[] observationTexts) {
         this.name = name;
         this.entities = entities;
-        this.textOutputs = textOutputs;
+        this.observationTexts = observationTexts;
     }
 }
 
 class Entity {
     String name;
-    TextOutput[] textOutputs;
+    ObservationText[] observationTexts;
+    ArrayList<Interaction> interactions;
 
-    public Entity(String name, TextOutput[] textOutputs) {
+    public Entity(String name, ObservationText[] observationTexts) {
         this.name = name;
-        this.textOutputs = textOutputs;
+        this.observationTexts = observationTexts;
+
     }
 }
 
-class Item{
-    String name;
-    TextOutput[] textOutputs;
-}
-
-class TextOutput{
+class ObservationText {
     String text;
-    TextCondition[] conditions;
+    ObservationCondition[] conditions;
 
-    public TextOutput(String text, TextCondition[] conditions) {
+    public ObservationText(String text, ObservationCondition[] conditions) {
         this.text = text;
         this.conditions = conditions;
     }
 }
 
-class TextCondition{
-    String name;
-
-    public TextCondition(String name) {
-        this.name = name;
-    }
+class ObservationCondition {
 
     public boolean isTrue(){
         return true;
     }
 }
 
-class HasItemCondition extends TextCondition{
+class HasItemCondition extends ObservationCondition {
 
     Item item;
 
-    public HasItemCondition(String name, Item item) {
-        super(name);
+    public HasItemCondition(Item item) {
         this.item = item;
     }
 
     public boolean isTrue(){
         return Main.ins.currentCharcter.items.contains(item);
+    }
+}
+
+class Item{
+    String name;
+    ObservationText[] observationTexts;
+    ArrayList<Interaction> interactions;
+
+    public Item(String name, ObservationText[] observationTexts) {
+        this.name = name;
+        this.observationTexts = observationTexts;
+    }
+}
+
+class Interaction{
+    String text;
+    Action[] actions;
+
+    public Interaction(String text, Action[] actions) {
+        this.text = text;
+        this.actions = actions;
+    }
+}
+
+class Action{
+    public void perform(){ }
+}
+
+class AddItemAction extends Action{
+    Item item;
+
+    public AddItemAction(Item item) {
+        this.item = item;
+    }
+
+    public void perform(){
+
+        Main.ins.currentCharcter.items.add(item);
+    }
+}
+
+class RemoveItemAction extends Action{
+    Item item;
+
+    public RemoveItemAction(Item item) {
+        this.item = item;
+    }
+
+    public void perform(){
+        ArrayList<Item> removeItems = new ArrayList<>();
+        for (Item testItem : Main.ins.currentCharcter.items) {
+            if(item == testItem){
+                removeItems.add(testItem);
+            }
+        }
+        Main.ins.currentCharcter.items.removeAll(removeItems);
     }
 }
